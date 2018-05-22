7ec9b9f573f99685245dfc9290f4433e61095fc7
alin damian <alin.damian94@gmail.com>
2018-05-22 11:15:00 -0700

    sound compresion good

--- src/main.py
+++ src/main.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     print(" * Panaitescu Cristian")
     print("``````````````````````````````````````````````")
 
-    textpredictors.test();
+    soundpredictors.test();
--- src/soundpredictors.py
+++ src/soundpredictors.py
@@ -5,6 +5,8 @@ from terminaltables import AsciiTable
 import wave # we use wave to open wav files for test
 import struct
 import constants
+import huffman as huff
+import ast, os
 
 # PREDICTORS
 def SAME(value):
@@ -16,6 +18,7 @@ def NEXT(value):
 def PREV(value):
     return value-1
 
+PREDICTORS = {"SAME" : SAME, "NEXT": NEXT, PREV: "PREV"}
 
 # compression function
 def compression(stream, predictor):
@@ -103,13 +106,161 @@ def run_singleTest(file):
                 results2.append(result)
         table = AsciiTable(results2)
         print(table.table)
-        
+
+#######################################################      
 def test():
     print("")
     print("This is a test for sound predictors")
     print("```````````````````````````````````")
 
-    testFiles = ['test.wav']
-    for testFile in testFiles:
-        print("Testing: " + testFile)
-        run_singleTest(testFile)
+    testFile = 'test.wav'
+    print("Testing: " + testFile)
+
+    saveTextHuffman(testFile, "test.wav.temp", "NEXT")
+    restoreFileData("test.wav.temp.arch")
+    
+########################################################
+
+def saveTextHuffman(inFilename, filename, predictor):
+
+    wavFile = wave.open(constants.RAW_PATH + inFilename)
+    numberOfChannels = wavFile.getnchannels()
+    numberOfFrames = wavFile.getnframes()
+    actualFrames = wavFile.readframes(numberOfFrames)
+    sampleWidth = wavFile.getsampwidth() # 2 for our case
+    sampleRate = wavFile.getframerate()
+    actualSampleArray = struct.unpack('h'*numberOfFrames,actualFrames)
+
+    stream = ""
+    (codings, residues) = getHuffman(actualSampleArray, predictor)
+
+    for residue in residues:
+        stream += codings[residue]
+
+    stream_file = open(filename, "w")
+    dict_file = open(filename + ".dict", "w")
+
+    stream_file.write(stream)
+    dict_file.write(predictor + "\n")
+    dict_file.write(str(numberOfChannels) + "\n")
+    dict_file.write(str(numberOfFrames) + "\n")
+    dict_file.write(str(sampleWidth) + "\n")
+    dict_file.write(str(sampleRate) + "\n")
+    dict_file.write(str(codings))
+
+    stream_file.close()
+    dict_file.close()
+
+    os.system(constants.EXECUTABLE + " u " + filename + " "  + filename + ".arch")
+    os.system("rm -f " + filename)
+
+def getHuffman(inStream, predictor):
+
+    residues = getRezidues(inStream, predictor)
+    num_appearances = {}
+    for residue in residues:
+        if residue not in num_appearances:
+            num_appearances[residue] = 1
+        else:
+            num_appearances[residue] += 1
+
+    huff_coll = []
+    for entry in num_appearances:
+        huff_coll += [(entry, num_appearances[entry])]
+
+    huff_coll = huff.codebook(huff_coll)
+    return (huff_coll, residues)
+
+
+
+def getRezidues(stream, predictor):
+    residues = [stream[0]]
+    predictorFunciton = PREDICTORS[predictor]
+
+    for i in range(0, len(stream) - 1):
+        currentValue = stream[i]
+        actualNextValue = stream[i+1]
+        
+
+        predictedValue = predictorFunciton(currentValue)
+        
+        residue = predictedValue - actualNextValue
+        residues.append(residue)
+
+    return residues
+
+def restoreValue(predictor, lastValue, residue):
+    predictorFunciton = PREDICTORS[predictor]
+    if lastValue is None:
+        return residue
+    return predictorFunciton(lastValue) - residue
+
+
+def restoreFileData(filename):
+
+    (residues, predictor, numberOfFrames, sampleWidth, numberOfChannels, sampleRate) = decodeResidues(filename)
+
+    fileData = []
+    lastValue = None
+    for residue in residues:
+        decodedValue = restoreValue(predictor, lastValue, residue)
+        lastValue = decodedValue
+        fileData.append(decodedValue)
+    
+    wavef = wave.open(constants.RAW_PATH + "test_restored.wav",'w')
+    wavef.setnchannels(numberOfChannels) # mono
+    wavef.setsampwidth(sampleWidth) 
+    wavef.setframerate(sampleRate)
+
+    for dataPiece in fileData:
+        data = struct.pack('<h', dataPiece)
+        wavef.writeframesraw( data )
+
+    wavef.writeframes('')
+    wavef.close()
+
+
+
+def decodeResidues(filename):
+
+    (codings, stream, predictor, numberOfFrames, sampleWidth, numberOfChannels, sampleRate) = loadTextHuffman(filename)
+    decodings = {}
+    residuums = []
+    for (key, value) in codings.iteritems():
+        decodings[value] = key
+
+    key = ""
+    index = 0
+    i = 0
+    while index < len(stream):
+        key += stream[index]
+        index += 1
+        if key in decodings:
+            residuums += [decodings[key]]
+            key = ""
+
+    return (residuums, predictor, numberOfFrames, sampleWidth, numberOfChannels, sampleRate)
+
+def loadTextHuffman(filename):
+
+    if ".arch" not in filename:
+        print "Not a valid format... We need .arch files"
+        return None
+
+    tmp_file = filename.split(".arch")[0]
+    os.system(constants.EXECUTABLE + " v " + filename + " " + tmp_file)
+ 
+    stream_file = open(tmp_file, "r")
+    dict_file = open(tmp_file + ".dict", "r")
+
+    stream = stream_file.read()
+    predictor = dict_file.readline().split('\n')[0]
+    numberOfChannels = ast.literal_eval(dict_file.readline())
+    numberOfFrames = ast.literal_eval(dict_file.readline())
+    sampleWidth = ast.literal_eval(dict_file.readline())
+    sampleRate = ast.literal_eval(dict_file.readline())
+    codings = ast.literal_eval(dict_file.read())
+    stream_file.close()
+    dict_file.close()
+
+    return (codings, stream, predictor, numberOfFrames, sampleWidth, numberOfChannels, sampleRate)
